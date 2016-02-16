04930a471eea59bdda68e7b0c02e25f42be77986
ngchwanlii <cng7@dons.usfca.edu>
2016-02-16 10:04:18 -0800

    import lastfm_subset

---
+++ Project/src/ParseArgument.java
@@ -0,0 +1,23 @@
+import java.nio.file.Path;
+
+public class ParseArgument {
+	
+	
+	private String inputFlag;
+	private String outputFlag;
+	private String orderFlag;
+	
+	private Path inputPath;
+	private Path outputPath;
+	
+	public ParseArgument(String[] args){
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
