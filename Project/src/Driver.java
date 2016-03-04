776e11e9ef55027d15d9f8754049f432a5dddca2
Sami Rollins <srollins@cs.usfca.edu>
2016-03-04 15:19:04 -0800

    changes required

--- Project/src/Driver.java
+++ Project/src/Driver.java
@@ -5,7 +5,8 @@ public class Driver {
 		try {
 			// if commandLineParser has exception, it will be catch under IllegalArgumentException 
 			ParseCommandLineArgs commandLineParser = new ParseCommandLineArgs(args);		
-						
+//TODO: pass in specific args rather than entire map.
+//TODO: move instantiation of SongDataProcessor here.
 			MusicLibrary musicLibrary = new MusicLibrary(commandLineParser.getArgsMap());			
 			musicLibrary.writeToTextFile();
 			
